6b7437d43113a5c4581f1c885ccf15a0614ec6a5
TashaKo12 <belinda.kovalyova@yandex.ru>
2022-05-12 20:02:50 +0300

    Изменила код

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,11 +1,19 @@
 from os import listdir
 import random
 
-import telegram
+from telegram.ext import Updater
 
 
-def send_picture(folders, TG_TOKEN, CHAT_ID):
+
+def random_path(folders):
     random_directory = random.choice(folders)
     random_picture = random.choice(listdir(random_directory))
-    bot = telegram.Bot(token=TG_TOKEN)
-    bot.send_document(chat_id=CHAT_ID, document=open(f'{random_directory}/{random_picture}', 'rb'))
+    return random_directory, random_picture
+    
+def send_picture(folders, TG_TOKEN, CHAT_ID):
+    random_directory, random_picture = random_path(folders)
+    updater = Updater(token=TG_TOKEN)
+    updater.bot.send_photo(
+        chat_id=CHAT_ID,
+        photo=open(f'{random_directory}/{random_picture}', 'rb')
+    )
