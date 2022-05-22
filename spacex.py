125b39bff258b97965853554727005cd496ed45f
TashaKo12 <belinda.kovalyova@yandex.ru>
2022-05-22 21:42:08 +0300

    Отредактировала SpaceX

--- spacex.py
+++ spacex.py
@@ -1,9 +1,15 @@
+import os
+import pathlib
+
 import requests
 
-import download_image
+from dotenv import load_dotenv
+from download_image import download_image
+
 
 
 def fetch_spacex_last_launch(folder_spacex):
+    
     image_link = "https://api.spacexdata.com/v3/launches"
 
     response = requests.get(image_link)
@@ -16,6 +22,19 @@ def fetch_spacex_last_launch(folder_spacex):
                 break
         for number, link in enumerate(image_list):
             file_path = f"{folder_spacex}/spacex{number}.jpg"
-            download_image.download_image(link, file_path)
+            download_image(link, file_path)
     except requests.exceptions.HTTPError as error:
         exit("Can't get data from server:\n{0}".format(error))
+
+
+def main():
+    load_dotenv()
+    
+    FOLDER_SPACEX = os.environ["FOLDER_SPACEX"]
+
+    pathlib.Path(FOLDER_SPACEX).mkdir(parents=True, exist_ok=True) 
+    fetch_spacex_last_launch(FOLDER_SPACEX)
+
+
+if __name__ == "__main__":
+	main()
