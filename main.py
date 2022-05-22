c037c0c87a4beb804ec5157131c01bac2976269d
TashaKo12 <belinda.kovalyova@yandex.ru>
2022-05-22 21:54:26 +0300

    Оформила main

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import os
-import pathlib
+
 import random
 from time import sleep
 from os import listdir
@@ -8,24 +8,20 @@ from dotenv import load_dotenv
 from telegram.ext import Updater
 
 
-def create_folders(folders):
-    for folder in folders:
-        pathlib.Path(folder).mkdir(parents=True, exist_ok=True) 
-
-
 def random_path(folders):
     random_directory = random.choice(folders)
     random_picture = random.choice(listdir(random_directory))
     return random_directory, random_picture
 
 
-def send_picture(folders, TG_TOKEN, CHAT_ID):
+def send_picture(folders, tg_token, chat_id):
     random_directory, random_picture = random_path(folders)
-    updater = Updater(token=TG_TOKEN)
+    updater = Updater(token=tg_token)
     updater.bot.send_photo(
-        chat_id=CHAT_ID,
+        chat_id=chat_id,
         photo=open(f'{random_directory}/{random_picture}', 'rb')
     )
+
 
 def main():
     
@@ -43,14 +39,12 @@ def main():
         FOLDER_SPACEX,
         FOLDER_EPIC
     ]
-	
-    create_folders(folders)
 
     seconds_in_one_day = 86400
     while True:
-        
         send_picture(folders, TG_TOKEN, CHAT_ID)
         sleep(seconds_in_one_day)
-    
+
+
 if __name__ == "__main__":
 	main()
