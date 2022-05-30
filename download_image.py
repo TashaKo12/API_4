4093db68ec073a36c9ea5c232b8e3edee4b99f77
TashaKo12 <belinda.kovalyova@yandex.ru>
2022-05-30 11:34:26 +0300

    Уточнила название переменной

--- download_image.py
+++ download_image.py
@@ -2,7 +2,7 @@ import requests
 
 
 def download_image(link, file_path, params = None):
-    link_image = requests.get(link, params = params)
-    link_image.raise_for_status()
+    response = requests.get(link, params = params)
+    response.raise_for_status()
     with open(file_path, 'wb') as file:
-        file.write(link_image.content)
+        file.write(response.content)
