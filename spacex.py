5b74292559eb059e27c6a079b5f3a3e7651f529b
TashaKo12 <belinda.kovalyova@yandex.ru>
2022-05-30 10:49:24 +0300

    Вынесла перехват ошибки за функцию

--- spacex.py
+++ spacex.py
@@ -15,16 +15,15 @@ def fetch_spacex_last_launch(folder_spacex):
     response = requests.get(image_link)
     response.raise_for_status()
     launches = response.json()
-    try:
-        for launch in launches:
-            if launch["links"]["flickr_images"]:
-                image_list = launch["links"]["flickr_images"]
-                break
-        for number, link in enumerate(image_list):
-            file_path = f"{folder_spacex}/spacex{number}.jpg"
-            download_image(link, file_path)
-    except requests.exceptions.HTTPError as error:
-        print("Can't get data from server:\n{0}".format(error))
+    
+    for launch in launches:
+        if launch["links"]["flickr_images"]:
+            image_list = launch["links"]["flickr_images"]
+            break
+    for number, link in enumerate(image_list):
+        file_path = f"{folder_spacex}/spacex{number}.jpg"
+        download_image(link, file_path)
+    
 
 
 def main():
@@ -32,8 +31,11 @@ def main():
     
     folder_spacex = os.environ["FOLDER_SPACEX"]
 
-    pathlib.Path(folder_spacex).mkdir(parents=True, exist_ok=True) 
-    fetch_spacex_last_launch(folder_spacex)
+    pathlib.Path(folder_spacex).mkdir(parents=True, exist_ok=True)
+    try:
+        fetch_spacex_last_launch(folder_spacex)
+    except requests.exceptions.HTTPError as error:
+        print("Can't get data from server:\n{0}".format(error))
 
 
 if __name__ == "__main__":
